a280bf63b7aa33c9d9a96835dbbb3a60381e4f2e
Aleksandr Denissov <137996362+Alex1988Den@users.noreply.github.com>
2024-12-30 12:50:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os
 import random
 import numpy as np
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, url_for
 from PIL import Image
 import logging
 
 # Инициализация приложения Flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Пути к папкам с изображениями
 folder1_path = '/content/drive/My Drive/datasets/impressionist/validation/validation'
 folder2_path = '/content/drive/My Drive/datasets/impressionist/training/training'
 
 # Папка для сохранения изображений
-static_images_path = '/content/drive/MyDrive/CV-Project-4/static/images'
+static_images_path = os.path.join(app.static_folder, "images")
 os.makedirs(static_images_path, exist_ok=True)
 
 # Настройка логирования
@@ -65,9 +65,17 @@ def blend():
         for i, w in enumerate(w_values):
             blended_image = blend_images(image1, image2, w)
             img_path = os.path.join(static_images_path, f"blended_{i}.png")
-            blended_image.save(img_path)
-            output_images.append(f"blended_{i}.png")  # Передаем только имя файла
-            app.logger.debug(f"Saved blended image {img_path}")
+
+            # Логирование пути сохранения
+            app.logger.debug(f"Saving blended image to {img_path}")
+
+            # Попытка сохранить изображение
+            try:
+                blended_image.save(img_path)
+                output_images.append(f"images/blended_{i}.png")  # Путь относительно static
+                app.logger.debug(f"Saved blended image {img_path}")
+            except Exception as e:
+                app.logger.error(f"Failed to save image {img_path}: {e}")
 
         # Логирование успешного завершения
         app.logger.debug("Blending images successful.")
